8b88fd51e3478fb227b734b4528b333a9cc52b3d
Sudeep <suddeepanand1986@gmail.com>
2021-01-27 08:50:16 +0100

    updated School class. added constructors

--- code/learnjava/src/learnjava/sk/School.java
+++ code/learnjava/src/learnjava/sk/School.java
@@ -4,11 +4,20 @@ public class School {
 
 	public String name;
 	public Student st = new Student();
-	
-	
+
+	public School() {
+
+	}
+
+	public School(String s, Student sd) {
+		name = s;
+		st = sd;
+
+	}
+
 	@Override
 	public String toString() {
-		return "School [name = "+name+", student = "+ st +"]";
+		return "School [name = " + name + ", student = " + st + "]";
 	}
-	 
+
 }
--- code/learnjava/src/learnjava/sk/collection/MainCollection.java
+++ code/learnjava/src/learnjava/sk/collection/MainCollection.java
@@ -5,48 +5,62 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import learnjava.sk.School;
 import learnjava.sk.Student;
 
 public class MainCollection {
 
 	public static void main(String[] args) {
 
-		
 		Student st = new Student("ABC", 111, 50.0, 60.0);
 		Student st2 = new Student("XYZ", 111, 50.0, 60.0);
-		
+		Student st3;
+		st3 = st2;
+		School sc = new School("DAV", st);
+
 		// Accepts duplicates, ordered or unordered
 		List l = new ArrayList();
-		
-		l.add(5);
-		l.add("Sarthak");
-		l.add(8);
-		l.add(st2);
-		l.add(30.87);
+
+//		l.add(5);
+//		l.add("Sarthak");
+//		l.add(8);
+//		l.add(st2);
+//		l.add(30.87);
 		l.add(st);
-		l.add(true);
-		
+//		l.add(true);
+		l.add(st2);
+		l.add(st3);
+		l.add(4 + 5);
+		l.add(4 * 6);
+		l.add(8 / 5);
+		l.add(sc);
 		for (int i = 0; i < 10; i++) {
-			
-			//l.add(i);
-			//System.out.println(i);
+
+			// l.add(i);
+			// System.out.println(i);
 		}
-		
+
 		System.out.println(l);
-		
 
 		// No duplicates
 		Set s = new HashSet();
-		s.add(5);
-		s.add(2);
-		s.add(8);
-		s.add(3);
-		s.add(l);
-		
+//		s.add(5);
+//		s.add(2);
+//		s.add(8);
+//		s.add(3);
+//		s.add(l);
+		s.add(st);
+		s.add(st2);
+		s.add(st3);
+		s.add(4 + 5);
+		s.add(9);
+		s.add(14);
+		s.add(4 * 6);
+		s.add(90 / 10);
+		s.add(8 / 5);
+		s.add(sc);
 		System.out.println(s);
-		
-		
-		
+
 	}
 
 }
